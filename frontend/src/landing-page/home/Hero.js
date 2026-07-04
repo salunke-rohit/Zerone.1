cf977c8c1c90d65241279d3032cde715b9bf4752
Rohit Salunke <rohitsalunke107@gmail.com>
2026-07-05 03:15:29 +0530

    new chages

--- frontend/src/landing-page/home/Hero.js
+++ frontend/src/landing-page/home/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 function Hero() {
     return ( 
         <div className='container p-5 '>
@@ -6,7 +7,7 @@ function Hero() {
                 <img  src='media\homeHero.png' alt='HeroImage' className='mb-5' />
                 <h1 className='mt-3' >Invest in everything</h1>
                 <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-                <button className="p-2 btn btn-primary fs-5 mb-5" style={{width:"20%", margin:"0 auto"}}>Signup Now </button>
+                <Link to="/signup"> <button  className="p-2 btn btn-primary fs-5 mb-5" style={{width:"20%", margin:"0 auto"}} >Signup Now </button></Link>
             </div>
         </div>
      );
